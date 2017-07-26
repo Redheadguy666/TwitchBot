ceaeea68b20b656f38b1f8bb2fb22c895723a463
Redheadguy666 <redheadguy666@gmail.com>
2017-07-26 12:06:20 +0300

    Initial commit

---
+++ toGitHub/settings.py
@@ -0,0 +1,12 @@
+HOST = "irc.twitch.tv"
+PORT = 6667
+NICK = # Your twitch account
+PASS = # Your auth token
+CHAN = # Channel
+
+SMTP_SERVER = # SMTP server
+SMTP_PORT = # SMTP port
+SMTP_LOGIN = # Email
+SMTP_PASSWORD = # Email password
+SMTP_MESSAGE = # Message you want to be sent
+
